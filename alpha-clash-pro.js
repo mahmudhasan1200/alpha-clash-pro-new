72d839300e4c1435822f555e13d36f7d3d30f8e8
mahmudhasan1200 <mahmudgfd792@gmail.com>
2024-08-25 02:17:55 +0600

    third commit

--- alpha-clash-pro.js
+++ alpha-clash-pro.js
@@ -10,4 +10,17 @@
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGaming()
+}
+
+function continueGaming(){
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet:', alphabet);
+
+    // Randomly generated alphabet to the screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set back-ground color 
+    setBackgroundColorById(alphabet);
 }
--- utility.js
+++ utility.js
@@ -3,7 +3,27 @@ function hideElementById(elementId){
     element.classList.add('hidden');
 }
 
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
 function showElementById(){
     const element = document.getElementById('play-ground');
     element.classList.remove('hidden');
+}
+
+function getARandomAlphabet(){
+    // get or create an alphabet Array 
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+    // get a random alphabet 
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
 }
